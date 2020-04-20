b20b131df5775eef68d03fea19307e2acbc646c8
nflukovtceva <57506522+nflukovtceva@users.noreply.github.com>
2020-04-20 08:15:30 -0700

    Add files via upload

--- add-form.js
+++ add-form.js
@@ -5,7 +5,11 @@ var path = require('path');
 
 
 function Add (req, res) {
+    var date = new Date(req.Acquiredt).toISOString().slice(0, 19).replace('T', ' ');
     
+    var sqlQuery1= `INSERT INTO Art(LocationID, Title, Type, Description, Collection, Status, Purchase_Price, Value, Acquiredt, Available, Media) VALUES('1000','${req.Title}', '${req.Type}', NULLIF('${req.DescriptionArt}', ''), NULLIF('${req.Collection}', ''), '${req.Status}', '${req.Purchase_Price}', '${req.Value}', '${date}', ${req.Available}, '${req.Media}');`
+    var sqlQuery2= `INSERT INTO Artist(LName, FName, Phone, Email, Address1, Address2, State, City, Zip, Shareholder) VALUES('${req.Lname}', '${req.Fname}', '${req.Phone}', '${req.Email}', '${req.Address1}', NULLIF('${req.Address2}', ''), '${req.State}', '${req.CityArtist}', '${req.Zip}', '${req.Shareholder[0]}');`
+    var sqlQuery3= `INSERT INTO Location(City, Building, Room, Description) VALUES('${req.CityLocation}', '${req.Building}', '${req.Room}', NULLIF('${req.DescriptionLocation}', ''));`
     var sql= require("mssql");
 
     var config = {
@@ -15,106 +19,38 @@ function Add (req, res) {
         database: '489_BAMS',
         port: 1433
     };
-
-    function addArtist(req){
-    var dbConn = new sql.ConnectionPool(config);
-
-        dbConn.connect().then(function () {
-
-            var transaction = new sql.Transaction(dbConn);
-            transaction.begin().then(function () {
-                var request = new sql.Request(transaction);
-                request.query(`INSERT INTO Artist(LName, FName, Phone, Email, Address1, Address2, State, City, Zip, Shareholder) VALUES('${req.Lname}', '${req.Fname}', '${req.Phone}', '${req.Email}', '${req.Address1}', '${req.Address2}', '${req.State}', '${req.CityArtist}', '${req.Zip}', '${req.Shareholder[0]}');`)
-            .then(function () {
-                    transaction.commit().then(function (recordSet) {
-                        //console.log(recordSet);
-                        dbConn.close();
-                    }).catch(function (err) {
-                        console.log("Error in Transaction Commit " + err);
-                        dbConn.close();
-                    });
-                }).catch(function (err) {
-                    console.log("Error in Transaction Begin " + err);
-                    dbConn.close();
-                });
-                
-            }).catch(function (err) {
-                console.log(err);
-                dbConn.close();
-            });
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
-
-    addArtist(req);
-
     function addArt(req){
-        console.log(req)
-        var dbConn = new sql.ConnectionPool(config);
-        
-            dbConn.connect().then(function () {
-        
-                var transaction = new sql.Transaction(dbConn);
-                transaction.begin().then(function () {
-                    var request = new sql.Request(transaction);
-                    var date = new Date(req.Acquiredt).toISOString().slice(0, 19).replace('T', ' ');
-                    request.query(`INSERT INTO Art(LocationID, Title, Type, Description, Collection, Status, Purchase_Price, Value, Acquiredt, Available, Media) VALUES('1000','${req.Title}', '${req.Type}', '${req.DescriptionArt}', '${req.Collection}', '${req.Status}', '${req.Purchase_Price}', '${req.Value}', '${date}', ${req.Available}, '${req.Media}');`)
-                .then(function () {
-                        transaction.commit().then(function (recordSet) {
-                            //console.log(recordSet);
-                            dbConn.close();
-                        }).catch(function (err) {
-                            console.log("Error in Transaction Commit " + err);
-                            dbConn.close();
-                        });
-                    }).catch(function (err) {
-                        console.log("Error in Transaction Begin " + err);
-                        dbConn.close();
-                    });
-                    
-                }).catch(function (err) {
-                    console.log(err);
-                    dbConn.close();
-                });
-            }).catch(function (err) {
-                console.log(err);
+        sql.connect(config, function (err) {
+            if (err) console.log(err.message);
+            let sqlRequest =  new sql.Request();	
+            sqlRequest.query(sqlQuery1, function (err,data) {	
+                if (err) console.log(err.message)
+                sql.close();			
             });
-        }  
-    addArt(req);
-
+        });}
+    function addArtist(req){
+    sql.connect(config, function (err) {
+		if (err) console.log(err.message);
+		let sqlRequest =  new sql.Request();	
+		sqlRequest.query(sqlQuery2, function (err,data) {	
+			if (err) console.log(err.message)
+			sql.close();			
+		});
+    });}
     function addLocation(req){
-        var dbConn = new sql.ConnectionPool(config);
-        
-            dbConn.connect().then(function () {
-        
-                var transaction = new sql.Transaction(dbConn);
-                transaction.begin().then(function () {
-                    var request = new sql.Request(transaction);
-                    request.query(`INSERT INTO Location(City, Building, Room, Description) VALUES('${req.CityLocation}', '${req.Building}', '${req.Room}', '${req.DescriptionLocation}');`)
-                .then(function () {
-                        transaction.commit().then(function (recordSet) {
-                            //console.log(recordSet);
-                            dbConn.close();
-                        }).catch(function (err) {
-                            console.log("Error in Transaction Commit " + err);
-                            dbConn.close();
-                        });
-                    }).catch(function (err) {
-                        console.log("Error in Transaction Begin " + err);
-                        dbConn.close();
-                    });
-                    
-                }).catch(function (err) {
-                    console.log(err);
-                    dbConn.close();
-                });
-            }).catch(function (err) {
-                console.log(err);
+        sql.connect(config, function (err) {
+            if (err) console.log(err.message);
+            let sqlRequest =  new sql.Request();	
+            sqlRequest.query(sqlQuery3, function (err,data) {	
+                if (err) console.log(err.message)
+                sql.close();			
             });
-        }  
+        });}
+    addArt(req);
+    addArtist(req);
     addLocation(req);
-
+    
+   
     var h='<h1 class="logo"></h1><h2>The data was added to the database.</h2>';
     var str='<link rel="stylesheet" type="text/css" href="../css/style.css"></link>';
     res.send(h+str)
