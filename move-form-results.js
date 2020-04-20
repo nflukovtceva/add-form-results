beb1b263e4c573d4da34fa9658860f745e75f339
nflukovtceva <57506522+nflukovtceva@users.noreply.github.com>
2020-04-20 09:07:28 -0700

    Add files via upload

--- move-form-results.js
+++ move-form-results.js
@@ -1,4 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var path = require('path');
+
 function MoveResults(req,res) {
+    var sqlQuery1 = `Select LocationID from Location Where Location.City= '${req.City}' 
+                                                    And Location.Building= '${req.Building}' 
+                                                    And Location.Room= '${req.Room}';`
+    var sqlQuery2 = `Update Art
+                    Set LocationID = 1000
+                    Where ArtID = ${req.ArtID}`
     var sql= require("mssql");
 
     var config = {
@@ -8,15 +19,20 @@ function MoveResults(req,res) {
         database: '489_BAMS',
         port: 1433
     };
-    
-    if(req.ButtonID===1){
-        var sqlQuery=`Select LocationID from Location
-						Where Location.City= '${req.City}'
-						And Location.Building= '${req.Building}'
-                        And Location.Room= '${req.Room}';`
-        var sqlQuery2=`Update Art
-                        Set LocationID = 1002
-                        Where ArtID = ${req.ArtID}`  
-                        }
+    function NewLocation(req, res) {
+        sql.connect(config, function (err) {
+            if (err) console.log(err.message);
+            let sqlRequest =  new sql.Request();	
+            sqlRequest.query(sqlQuery1, sqlQuery2, function (err,data) {	
+                if (err) console.log(err.message)
+                sql.close();			
+            });
+        });}
+    NewLocation(req);
+              
+    var h='<h1 class="logo"></h1><h2>The data was added to the database.</h2>';
+    var str='<link rel="stylesheet" type="text/css" href="../css/style.css"></link>';
+    res.send(h+str)
+
 };
-module.exports=MoveResults
+module.exports=MoveResults 
